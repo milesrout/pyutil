cc8c9457b092684588c24d82cb83583dc6688cbc
Miles Rout <miles.rout@gmail.com>
2016-09-18 14:27:38 +1200

    Call __init__ in constructor

--- util.py
+++ util.py
@@ -3,11 +3,12 @@ import functools, inspect, sys
 def constructor(__init__):
     @functools.wraps(__init__)
     def new_init(self, *args, **kwds):
-        bound_args = inspect.signature(__init__).bind(*args, **kwds)
+        bound_args = inspect.signature(__init__).bind(self, *args, **kwds)
         if sys.version_info >= (3,5):
             bound_args.apply_defaults()
         for k, v in bound_args.arguments.items():
             setattr(self, k, v)
+        __init__(self, *args, **kwds)
     return new_init
     
 def accumulate(accum_type):
